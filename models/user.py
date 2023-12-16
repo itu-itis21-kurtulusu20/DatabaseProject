0b49bb69cd4713fc7f5d6d47bb32d22bdc45cee6
Uğurcan Kurtuluş <92296838+itu-itis21-kurtulusu20@users.noreply.github.com>
2023-12-16 18:18:39 +0300

    Updated user.py, edited the inputs.

--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
 from flask import current_app
 from flask_login import UserMixin
 
-
+# Flask-Login assumes that there will be a unique string value for identifying each user.
+# In most cases, this will be the string value of the database id number for a user. 
+# In our example, we will use the username for this purpose. 
 class User(UserMixin):
     def __init__(self, username, password):
         self.username = username
@@ -15,7 +17,6 @@ class User(UserMixin):
     @property
     def is_active(self):
         return self.active
-
 
 def get_user(user_id):
     password = current_app.config["PASSWORDS"].get(user_id)
